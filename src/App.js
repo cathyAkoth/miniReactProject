0fa70cb4afef3185f4a35da04108d932bb71e44f
CathyAkoth <catherinececilia22@gmail.com>
2021-10-31 04:46:00 -0700

    Added main Navigation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import {Route, Switch} from 'react-router-dom';
 
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
-import FavoritesPage from './pages/Favorites';
+import FavoritesPage from './pages/Favorites'; 
+import MainNavigation from './components/layout/MainNavigation';
 function App(){
     return(<div>
+        <MainNavigation />
         <Switch>
         <Route path='/' exact={true}>
             <AllMeetupsPage />
         </Route>
-        <Route path=' '>
+        <Route path='/new-meetups'>
             <NewMeetupPage />
         </Route>
         <Route path='/favorites'>
