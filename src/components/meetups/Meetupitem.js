980a67e7d2823186b1cb09f75bd1604d1f39fb66
CathyAkoth <catherinececilia22@gmail.com>
2021-11-01 11:56:07 -0700

    Added Card.js

--- src/components/meetups/Meetupitem.js
+++ src/components/meetups/Meetupitem.js
@@ -1,8 +1,10 @@
+import Card from '../ui/Card';
 import classes from './Meetupitem.module.css';
 
 function Meetupitem(props){
 return (
 <li className={classes.item}>
+ <Card>
     <div className={classes.image}>
         <img src="{props.image}" alt="{props.title}" />
     </div>
@@ -14,6 +16,7 @@ return (
     <div className={classes.actions}>
         <button>To Favorites</button>
     </div>
+ </Card>
 </li>
 );
 
--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,7 +1,7 @@
 import classes from './Card.module.css';
 
 function Card(){
-    return <div className={classes.card}></div>
+    return <div className={classes.card}></div>;
 }
 
 export default Card;
